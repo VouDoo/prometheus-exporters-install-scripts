0717314af992dfe1797f284f814b452f28496fda
Maxence <maxgrymonprez@live.fr>
2021-11-18 14:42:21 +0000

    Improve Windows Exporter script

--- Install-WindowsExporter.ps1
+++ Install-WindowsExporter.ps1
@@ -1,4 +1,3 @@
-#Requires -Version 2.0
 #Requires -RunAsAdministrator
 
 <#
@@ -7,13 +6,37 @@
 Install Windows Exporter
 
 .DESCRIPTION
-This script automatically downloads and installs Windows Exporter.
+Automatically download and install Windows Exporter.
+
+.PARAMETER Version
+Version of Windows Exporter.
+Find out the latest release of Windows exporter on GitHub:
+    https://github.com/prometheus-community/windows_exporter/releases
+
+.PARAMETER Architecture
+System architecture.
+Find out the architectures supported by Windows exporter on GitHub:
+    https://github.com/prometheus-community/windows_exporter/releases
+
+.PARAMETER Collectors
+Comma-separated list of collectors to use in Windows Exporter.
+Find out the Windows Exporter collectors on GitHub:
+    https://github.com/prometheus-community/windows_exporter#collectors
+
+.PARAMETER InstallDirectoryPath
+Path to the installation directory.
+
+.PARAMETER FirewallProfile
+Profile to which the Windows firewall rule is assigned.
 
 .EXAMPLE
 .\Install-WindowsExporter.ps1
 
+.EXAMPLE
+.\Install-WindowsExporter.ps1 -Version "0.16.0" -Architecture "amd64" -InstallDirectoryPath "C:\prometheus" -FirewallProfile Domain
+
 .NOTES
-(C) Copyright 2019, Maxence Grymonprez <maxgrymonprez@live.fr>
+(C) Copyright 2021, Maxence Grymonprez <maxgrymonprez@live.fr>
 
 Install-WindowsExporter.ps1 is free software: you can redistribute
 it and/or modify it under the terms of the GNU General Public License
@@ -29,78 +52,98 @@ You should have received a copy of the GNU General Public License
 along with help.sh. If not, see <http://www.gnu.org/licenses>.
 
 .LINK
-PowerShell online documentation:
-https://docs.microsoft.com/en-us/powershell
-
-Windows Exporter GitHub repository:
-https://github.com/prometheus-community/windows_exporter
+- PowerShell online documentation:
+    https://docs.microsoft.com/en-us/powershell
+- Windows Exporter GitHub repository:
+    https://github.com/prometheus-community/windows_exporter
 
 #>
 
-# Set verbose preference
-Set-Variable -Name VerbosePreference -Value "Continue" -Scope Global
+[CmdletBinding()]
+param (
+    [Parameter(HelpMessage = "Version of Windows Exporter")]
+    [ValidateNotNullOrEmpty()]
+    [string] $Version = "0.16.0",
 
-# You can replace the version number and the architecture name.
-# Find out the latest release of Windows exporter on the official
-# GitHub repository: https://github.com/prometheus-community/windows_exporter/releases
-# Version of Windows Exporter
-$Version = "0.16.0"
-# System architecture
-$Arch = "amd64"
+    [Parameter(HelpMessage = "System architecture")]
+    [ValidateNotNullOrEmpty()]
+    [string] $Architecture = "amd64",
 
-# Check if Windows Exporter is already in place
-if (Test-Path -Path "C:\prometheus\windows_exporter.exe" -PathType Leaf) {
-    throw "windows_exporter.exe already exists in C:\Prometheus\"
+    [Parameter(HelpMessage = "Comma-separated list of collectors to use in Windows Exporter")]
+    [ValidateNotNullOrEmpty()]
+    [string] $Collectors = "cpu,cs,logical_disk,net,os,service,system,textfile,tcp,process",
+
+    [Parameter(HelpMessage = "Path to the installation directory")]
+    [ValidateNotNullOrEmpty()]
+    [Alias("Path")]
+    [string] $InstallDirectoryPath = "C:\prometheus",
+
+    [Parameter(HelpMessage = "Profile to which the Windows firewall rule is assigned")]
+    [ValidateSet("Domain", "Private", "Public", "Any")]
+    [string] $FirewallProfile = "Domain"
+)
+
+begin {
+    $ErrorActionPreference = "Stop"
 }
 
-# Create prometheus directory if it does not exist
-if (-not (Test-Path -Path "C:\prometheus" -PathType Container)) {
-    Write-Verbose -Message "Create Prometheus directory"
-    New-Item -ItemType Directory -Path "C:\prometheus"
+process {
+    $InstallDirectoryPath = [System.IO.Path]::GetFullPath($InstallDirectoryPath)
+
+    $Service = @{
+        Name           = "PrometheusWindowsExporter"
+        DisplayName    = "Prometheus Windows Exporter"
+        Description    = "Export Windows system metrics for Prometheus"
+        BinaryPathName = "`"$InstallDirectoryPath\windows_exporter.exe`" --collectors.enabled=`"$Collectors`" --collector.textfile.directory=`"$InstallDirectoryPath\textfile_inputs`""
+        StartupType    = "Automatic"
+    }
+
+    $NetFirewallRule = @{
+        Name        = "prometheus_windows_exporter"
+        DisplayName = "Prometheus Windows Exporter"
+        Protocol    = "TCP"
+        LocalPort   = 9182
+        Group       = "Prometheus"
+        Action      = "Allow"
+        Direction   = "Inbound"
+        Profile     = $FirewallProfile
+    }
+
+    if (Test-Path -Path "$InstallDirectoryPath\windows_exporter.exe" -PathType Leaf) {
+        throw "`"$InstallDirectoryPath\windows_exporter.exe`" already exists."
+    }
+
+    if (-not (Test-Path -Path $InstallDirectoryPath -PathType Container)) {
+        Write-Verbose -Message "Create `"$InstallDirectoryPath`" directory."
+        New-Item -Path $InstallDirectoryPath -ItemType Directory -Force | Out-Null
+    }
+
+    Write-Verbose -Message "Download executable."
+    try {
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        (New-Object System.Net.WebClient).DownloadFile(
+            "https://github.com/prometheus-community/windows_exporter/releases/download/v$Version/windows_exporter-$Version-$Architecture.exe",
+            "$InstallDirectoryPath\windows_exporter.exe"
+        )
+    }
+    catch {
+        throw "An error occurred while downloading: {0}" -f $_.Exception.Message
+    }
+
+    if (-not (Test-Path -Path "$InstallDirectoryPath\textfile_inputs" -PathType Container)) {
+        Write-Verbose -Message "Create `"textfile_inputs`" subdirectory."
+        New-Item -Path "$InstallDirectoryPath\textfile_inputs" -ItemType Directory | Out-Null
+    }
+
+    Write-Verbose -Message "Create Windows service."
+    New-Service @Service | Out-Null
+
+    Write-Verbose -Message "Set Windows service auto-restart on failure."
+    & sc.exe failure $Service.Name reset= 30 actions= restart/5000 | Out-Null
+
+    Write-Verbose -Message "Start Windows Service."
+    Start-Service -Name $Service.Name
+
+    Write-Verbose -Message "Add Windows firewall rule."
+    New-NetFirewallRule @NetFirewallRule | Out-Null
 }
-
-# Download Windows Exporter
-Write-Verbose -Message "Download Windows Exporter"
-try {
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    (New-Object System.Net.WebClient).DownloadFile(
-        "https://github.com/prometheus-community/windows_exporter/releases/download/v$Version/windows_exporter-$Version-$Arch.exe",
-        "C:\prometheus\windows_exporter.exe"
-    )
-} catch {
-    throw ("An error occurred during the download of Windows Exporter. Error message: " + $_.Exception.Message)
-}
-
-# Create inputs directory for Windows Exporter if it does not exist
-if (-not (Test-Path -Path "C:\prometheus\textfile_inputs" -PathType Container)) {
-    Write-Verbose -Message "Create textfile_inputs directory for Windows Exporter"
-    New-Item -ItemType Directory -Path "C:\prometheus\textfile_inputs"
-}
-
-# You can change the Windows Exporter collectors if required,
-# more information on https://github.com/prometheus-community/windows_exporter#collectors
-$WindowsExporterCollectors = "cpu,cs,logical_disk,net,os,service,system,textfile,tcp,process"
-# Create Windows service
-Write-Verbose -Message "Create Windows service for Windows Exporter"
-New-Service -Name PrometheusWindowsExporter `
-    -DisplayName "Prometheus Windows Exporter" `
-    -Description "Export Windows system metrics for Prometheus" `
-    -BinaryPathName "`"C:\\prometheus\\windows_exporter.exe`" --collectors.enabled=`"$WindowsExporterCollectors`" --collector.textfile.directory=`"C:\\prometheus\\textfile_inputs`""
-
-# You can change the Windows firewall rule profile if required,
-# more information on https://docs.microsoft.com/en-us/powershell/module/netsecurity/new-netfirewallrule
-$FWRuleProfile = "Domain"
-# Add Windows firewall rule
-Write-Verbose -Message "Add Windows firewall rule for Windows Exporter"
-New-NetFirewallRule -Name "prometheus_windows_exporter" `
-    -DisplayName "Prometheus Windows Exporter" `
-    -Protocol TCP `
-    -LocalPort 9182 `
-    -Group "Prometheus" `
-    -Action Allow `
-    -Direction Inbound `
-    -Profile  $FWRuleProfile
-
-# Start Windows Service
-Write-Verbose -Message "Start Windows Exporter Windows service"
-Start-Service -Name PrometheusWindowsExporter
